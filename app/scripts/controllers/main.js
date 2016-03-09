57873fc1e62335be4818853dcd2e8a707b9b3507
fernando.supi <fernando.supi@gmail.com>
2016-03-09 09:45:07 -0400

    general

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mapiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mapiApp
+ */
+angular.module('mapiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
