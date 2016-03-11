41559c62dea86330f41595fd126e93ad1e86dbea
fernando.supi <fernando.supi@gmail.com>
2016-03-11 19:06:26 -0400

    utilizacao de jsonp para acesso CORS no endereco original, testes de adicao de headers CORS

--- app/scripts/controllers/cep.js
+++ app/scripts/controllers/cep.js
@@ -10,13 +10,30 @@
 
 angular.module('mapiApp').controller('CepCtrl', function($http, $scope){
 
+    
+    var app = angular.module('mapiApp',[]);
+    
+    app.config(function($httpProvider) {
+      //Enable cross domain calls
+      $httpProvider.defaults.useXDomain = true;
+ 
+      //Remove the header used to identify ajax call  that would prevent CORS from working
+      delete $httpProvider.defaults.headers.common['X-Requested-With'];
+      $httpProvider.defaults.headers.common["Access-Control-Allow-Origin"] = "*";
+      $httpProvider.defaults.headers.common["Accept"] = "application/json";
+      $httpProvider.defaults.headers.common["content-type"] = "application/json";
+      $httpProvider.defaults.headers.common['X-User-Agent'] = "mapiApp";
+    });
+    
+
     var urlMapp = 'http://localhost:8080/mapi/endereco';
-    //var urlWebCep = 'http://correiosapi.apphb.com/cep/';
-    var urlWebCep = 'http://cep.correiocontrol.com.br/';
+    var urlWebCep = 'http://correiosapi.apphb.com/cep/';
+    //var urlWebCep = 'http://cep.correiocontrol.com.br/';
 
     $scope.buscaCepWeb = function(){
-        $http.get(urlWebCep + $scope.cep.codigo + '.json')
+        //$http.get(urlWebCep + $scope.cep.codigo + '.json')
         //$http.get(urlWebCep + $scope.cep.codigo)
+        $http.jsonp(urlWebCep + $scope.cep.codigo + '?callback=JSON_CALLBACK')
         .success(function(local){
             $scope.local_encontrado = local;
         console.log(local);
